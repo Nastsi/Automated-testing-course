5acd3c154361ae324d97ddbd8f30db2fdaa4948c
Anastacia Pischikova <apischikova@beetsoft.com>
2019-05-30 15:56:08 +0600

    Creating a project

---
+++ automated-testing-course/src/main/java/com/beetsoft/automated/App.java
@@ -0,0 +1,13 @@
+package com.beetsoft.automated;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
