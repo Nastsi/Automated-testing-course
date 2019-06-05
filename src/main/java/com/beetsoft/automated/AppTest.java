1560a87e36ec2239adb1ed621400428fea49487a
Anastacia Pischikova <apischikova@beetsoft.com>
2019-06-05 11:22:55 +0600

    adding junit

--- src/main/java/com/beetsoft/automated/AppTest.java
+++ src/main/java/com/beetsoft/automated/AppTest.java
@@ -1,12 +1,15 @@
 package com.beetsoft.automated;
 
 
+import org.junit.Test;
+
 /**
  * Hello world!
  *
  */
 public class AppTest
 {
+    @Test
     public static void main(String[] args)
     {
         System.out.print("Hello world");
